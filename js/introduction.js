a75cc06c0d74eded5d0801246e421d8206cfce80
khanahmedm <khan.ahmed.m@outlook.com>
2024-06-09 23:19:35 -0500

    added introduction

---
+++ js/introduction.js
@@ -0,0 +1,27 @@
+async function fetchJSONData() {
+    try {
+        const response = await fetch('../json/introduction.json');
+        const data = await response.json();
+        displayIntro(data);
+    } catch (error) {
+        console.error('Error fetching the JSON data:', error);
+    }
+}
+
+// Function to display JSON data in the HTML
+function displayIntro(data) {
+    const intro = document.getElementById('intro');
+    //authorInfo.style.fontSize = '5px'
+
+    data.forEach(user => {
+        const introDiv = document.createElement('div');
+        //userDiv.className = 'user';
+        introDiv.classList.add('intro');
+        //userDiv.innerHTML = `<h1>${user.name}</h1><a c href="mailto:${user.email}>${user.email}</a>`;
+        introDiv.innerHTML = `<p>${user.introduction}</p>`;
+        intro.appendChild(introDiv);
+    });
+}
+
+// Fetch JSON data when the page loads
+document.addEventListener('DOMContentLoaded', fetchJSONData);
