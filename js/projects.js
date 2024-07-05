e641f62291519835193744e87dd08f3a8efd938f
khanahmedm <khan.ahmed.m@outlook.com>
2024-07-04 21:27:22 -0500

    modified code for projects tiles and modals

--- js/projects.js
+++ js/projects.js
@@ -1,54 +1,94 @@
-async function fetchJSONData() {
-    try {
-        const response = await fetch('../json/projects.json');
-        const data = await response.json();
-        displayProjects(data);
-    } catch (error) {
-        console.error('Error fetching the JSON data:', error);
-    }
-}
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('../json/projects.json')
+        .then(response => response.json())
+        .then(data => {
+            displayProjects(data);
+        })
+        .catch(error => console.error('Error fetching projects:', error));
+});
 
-// Function to display JSON data in the HTML
-function displayProjects(data) {
-    const projectList = document.getElementById('projects-list');
-    
-    data.forEach(project => {
-        const projectDiv = document.createElement('div');
-        projectDiv.classList.add('project-item');
-        projectDiv.innerHTML = `
-                            <table>
-                                <tr>
-                                    <td class="col-img"><a href="http://${project.url}"><img src="images/${project.image}" alt="${project.client}"></a></td>
-                                    <td class="col-text">
-                                        <h3>Project: ${project.project}</h3>
-                                        <p><b>Client:</b> ${project.client}</p>
-                                        <p><b>Role:</b> ${project.role}</p>
-                                        <p><b>Year:</b> ${project.year}</p>
-                                        <p><b>Skills:</b> ${project.skills}</p>
-                                        <br>
-                                        <p><b>Description:</b></p>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td>
-                                    <ul><li>
-                                    ${project.description.replace(/\n/g, "<br><br><li>")}
-                                    </ul>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td>
-                                    <iframe src="html/${project.diagram}" width="100%" height="450px" frameborder="0"></iframe>
-                                    </td>
-                                </tr>
-                            </table>
-                            <hr>
-                        `;
-        projectList.appendChild(projectDiv);
+function displayProjects(projects) {
+    var projectsContainer = document.getElementById('projects-container');
+
+    projects.forEach(function(project) {
+        var tile = document.createElement('div');
+        tile.classList.add('project-tile');
+
+        var diagram = document.createElement('div');
+        diagram.classList.add('project-diagram');
+        diagram.innerHTML = "<img src='images/" + project.diagram_image + "'>";
+
+        var title = document.createElement('div');
+        title.classList.add('project-title');
+        title.textContent = project.project;
+
+        var client = document.createElement('div');
+        client.classList.add('project-client');
+        client.textContent = project.client;
+
+        var year = document.createElement('div');
+        year.classList.add('project-year');
+        year.textContent = project.year;
+
+        tile.appendChild(diagram);
+        tile.appendChild(title);
+        tile.appendChild(client);
+        tile.appendChild(year);
+
+        tile.addEventListener('click', function() {
+            openModal(project);
+        });
+
+        projectsContainer.appendChild(tile);
     });
 }
 
-// Fetch JSON data when the page loads
-document.addEventListener('DOMContentLoaded', fetchJSONData);
+function openModal(project) {
+    var modalContainer = document.getElementById('modal-container');
+
+    var modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    var modalContent = document.createElement('div');
+    modalContent.classList.add('modal-content');
+
+    var closeBtn = document.createElement('span');
+    closeBtn.classList.add('close');
+    closeBtn.innerHTML = '&times;';
+    closeBtn.addEventListener('click', function() {
+        modal.style.display = 'none';
+    });
+
+    var modalTitle = document.createElement('h2');
+    modalTitle.innerHTML = project.project + "<img class='logo' src='images/" + project.image + "'>";
+
+    var modalClient = document.createElement('p');
+    modalClient.innerHTML = "Client: <b>" + project.client + "</b>";
+
+    var modalDetails = document.createElement('p');
+    modalDetails.innerHTML = "Details: <ul><li> " + project.description.replace(/\n/g, "<br><br><li>") + "</ul>";
+
+    var modalArchitecture = document.createElement('p');
+    modalArchitecture.innerHTML = "<iframe src='html/" + project.diagram + "' width='100%' height='700px' frameborder='0'></iframe>"
+
+
+
+    modalContent.appendChild(closeBtn);
+    modalContent.appendChild(modalTitle);
+    modalContent.appendChild(modalClient);
+    modalContent.appendChild(modalDetails);
+    modalContent.appendChild(modalArchitecture);
+    modal.appendChild(modalContent);
+
+    modal.addEventListener('click', function(event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+    
+    modalContainer.innerHTML = ''; // Clear previous content
+    modalContainer.appendChild(modal);
+
+    //modal.style.display = 'block';
+    modal.style.display = 'flex';
+}
