c8c5dfa83d34c6fa8c811546df5c361274f162ff
khanahmedm <khan.ahmed.m@outlook.com>
2025-02-23 22:57:13 -0600

    nav

--- js/nav.js
+++ js/nav.js
@@ -1,25 +1,25 @@
 //document.addEventListener('DOMContentLoaded', function () {
-    window.addEventListener('load', function () {
-        const navLinks = document.querySelectorAll('.nav-link');
-    
-        console.log(navLinks);
-        // Add scroll event listener
-        window.addEventListener('scroll', function () {
-            let fromTop = window.scrollY + 10;
-    
-            navLinks.forEach(link => {
-                let section = document.querySelector(link.hash);
-    
-                console.log(link.hash);
-                // Check if current scroll position is within section
-                if (
-                    section.offsetTop <= fromTop &&
-                    section.offsetTop + section.offsetHeight > fromTop
-                ) {
-                    link.classList.add('active');
-                } else {
-                    link.classList.remove('active');
-                }
-            });
+window.addEventListener('load', function () {
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    console.log(navLinks);
+    // Add scroll event listener
+    window.addEventListener('scroll', function () {
+        let fromTop = window.scrollY + 10;
+
+        navLinks.forEach(link => {
+            let section = document.querySelector(link.hash);
+
+            console.log(link.hash);
+            // Check if current scroll position is within section
+            if (
+                section.offsetTop <= fromTop &&
+                section.offsetTop + section.offsetHeight > fromTop
+            ) {
+                link.classList.add('active');
+            } else {
+                link.classList.remove('active');
+            }
         });
     });
+});
