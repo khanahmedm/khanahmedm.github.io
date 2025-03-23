62a9a20be96db94989aba60612148b786549f0bf
khanahmedm <khan.ahmed.m@outlook.com>
2025-03-23 01:00:30 -0500

    filter on show attribute

--- js/certifications.js
+++ js/certifications.js
@@ -13,22 +13,24 @@ function displayCertification(data) {
     const certificationList = document.getElementById('certifications-list');
     
     data.forEach(certification => {
-        const certificationDiv = document.createElement('div');
-        certificationDiv.classList.add('certification-item');
-        certificationDiv.innerHTML = `
-                            <table>
-                                <tr>
-                                    <td class="col-img"><a href="http://${certification.url}"><img src="images/${certification.image}" alt="${certification.certification}"></a></td>
-                                    <td class="col-text">
-                                        <h3>${certification.certification}</h3>
-                                        <p>Issued By: ${certification.issuedBy}</p>
-                                        <p>Issue Date: ${certification.issuedDate}</p>
-                                        <p>Expiration Date: ${certification.expirationDate}</p>
-                                    </td>
-                                </tr>
-                            </table>
-                        `;
-        certificationList.appendChild(certificationDiv);
+        if (certification.show && certification.show.toLowerCase() === "yes") {
+            const certificationDiv = document.createElement('div');
+            certificationDiv.classList.add('certification-item');
+            certificationDiv.innerHTML = `
+                                <table>
+                                    <tr>
+                                        <td class="col-img"><a href="http://${certification.url}"><img src="images/${certification.image}" alt="${certification.certification}"></a></td>
+                                        <td class="col-text">
+                                            <h3>${certification.certification}</h3>
+                                            <p>Issued By: ${certification.issuedBy}</p>
+                                            <p>Issue Date: ${certification.issuedDate}</p>
+                                            <p>Expiration Date: ${certification.expirationDate}</p>
+                                        </td>
+                                    </tr>
+                                </table>
+                            `;
+            certificationList.appendChild(certificationDiv);            
+        }
     });
 }
 
