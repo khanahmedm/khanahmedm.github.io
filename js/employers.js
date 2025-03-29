44aeabf03c90a05115ab0a9c8ed8b5fb40f12a48
khanahmedm <khan.ahmed.m@outlook.com>
2025-03-29 10:46:10 -0500

    added filter for show attribute

--- js/employers.js
+++ js/employers.js
@@ -3,8 +3,8 @@ async function fetchJSONData() {
         const response = await fetch('../json/employers.json');
         const data = await response.json();
         displayEmployers(data);
-    } 
-    
+    }
+
     catch (error) {
         console.error('Error fetching the JSON data:', error);
     }
@@ -12,7 +12,7 @@ async function fetchJSONData() {
     const showMoreLinks = document.querySelectorAll('.show-more');
 
     showMoreLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
+        link.addEventListener('click', function (event) {
             event.preventDefault();
             const details = this.previousElementSibling;
 
@@ -52,105 +52,107 @@ async function fetchJSONData() {
 // Function to display JSON data in the HTML
 function displayEmployers(data) {
     const employersList = document.getElementById('employers-list');
-    
+
     const table = document.createElement('table');
     table.className = 'table';
     table.classList.add('work-item');
-    
-    data.forEach(employer => {
-        const row = document.createElement('tr');
 
-        const imgCell = document.createElement('td');
-        imgCell.classList.add('col-img');
-        const img = document.createElement('img');
-        
-        img.src = "images/" + employer.image; // assuming the JSON has an image URL
-        img.alt = employer.company;
-        img.href = employer.url;
-        //img.style.width = '50px'; // adjust as necessary
-        //img.style.textAlign = 'center'
-        imgCell.appendChild(img);
+    data
+        .filter(employer => employer.show && employer.show.toLowerCase() === 'yes')
+        .forEach(employer => {
+            const row = document.createElement('tr');
 
-        const employerCell = document.createElement('td');
-        employerCell.classList.add('col-text');
-        const heading = document.createElement('h3');
-        heading.textContent = employer.company;
+            const imgCell = document.createElement('td');
+            imgCell.classList.add('col-img');
+            const img = document.createElement('img');
 
-        const organization = document.createElement('h4');
-        organization.textContent = employer.organization;
+            img.src = "images/" + employer.image; // assuming the JSON has an image URL
+            img.alt = employer.company;
+            img.href = employer.url;
+            //img.style.width = '50px'; // adjust as necessary
+            //img.style.textAlign = 'center'
+            imgCell.appendChild(img);
 
-        const role = document.createElement('p');
-        role.innerHTML = 'Title: <b>' + employer.role + '</b>';
-        //role.style.fontWeight = 'bolder'
+            const employerCell = document.createElement('td');
+            employerCell.classList.add('col-text');
+            const heading = document.createElement('h3');
+            heading.textContent = employer.company;
 
-        const duration = document.createElement('p');
-        duration.textContent = 'Duration: ' + employer.duration;
+            const organization = document.createElement('h4');
+            organization.textContent = employer.organization;
 
-        const location = document.createElement('p');
-        location.textContent = 'Location: ' + employer.location;
+            const role = document.createElement('p');
+            role.innerHTML = 'Title: <b>' + employer.role + '</b>';
+            //role.style.fontWeight = 'bolder'
 
-        const details = document.createElement('p');
-        details.textContent = 'Details: ';
-        
-        const description = document.createElement('ul');
-        description.className = 'details';
-        //details.innerHTML = "<li> " + employer.description.replace(/\n/g, "<br><br><li>");
-        employer.description.forEach(detail => {
-            const listItem = document.createElement('li');
-            listItem.innerHTML = detail;
-            description.appendChild(listItem);
+            const duration = document.createElement('p');
+            duration.textContent = 'Duration: ' + employer.duration;
+
+            const location = document.createElement('p');
+            location.textContent = 'Location: ' + employer.location;
+
+            const details = document.createElement('p');
+            details.textContent = 'Details: ';
+
+            const description = document.createElement('ul');
+            description.className = 'details';
+            //details.innerHTML = "<li> " + employer.description.replace(/\n/g, "<br><br><li>");
+            employer.description.forEach(detail => {
+                const listItem = document.createElement('li');
+                listItem.innerHTML = detail;
+                description.appendChild(listItem);
+            });
+
+            const skills = document.createElement('p');
+            //skills.className = 'details'
+            skills.innerHTML = '<br><b>Skills:</b> ' + employer.skills;
+            description.appendChild(skills);
+
+            const showMore = document.createElement('a');
+            showMore.href = '#';
+            showMore.className = 'show-more';
+            showMore.textContent = 'Show More';
+
+            employerCell.appendChild(heading);
+            employerCell.appendChild(organization);
+            employerCell.appendChild(role);
+            employerCell.appendChild(duration);
+            employerCell.appendChild(location);
+            employerCell.appendChild(details);
+            employerCell.appendChild(description);
+            //employerCell.appendChild(skills);
+            employerCell.appendChild(showMore);
+
+            row.appendChild(imgCell);
+            row.appendChild(employerCell);
+            table.appendChild(row);
         });
-        
-        const skills = document.createElement('p');
-        //skills.className = 'details'
-        skills.innerHTML = '<br><b>Skills:</b> ' + employer.skills;
-        description.appendChild(skills);
-
-        const showMore = document.createElement('a');
-        showMore.href = '#';
-        showMore.className = 'show-more';
-        showMore.textContent = 'Show More';
-    
-        employerCell.appendChild(heading);
-        employerCell.appendChild(organization);
-        employerCell.appendChild(role);
-        employerCell.appendChild(duration);
-        employerCell.appendChild(location);
-        employerCell.appendChild(details);
-        employerCell.appendChild(description);
-        //employerCell.appendChild(skills);
-        employerCell.appendChild(showMore);
-
-        row.appendChild(imgCell);
-        row.appendChild(employerCell);
-        table.appendChild(row);
-    });
 
     employersList.appendChild(table);
 
-/*    data.forEach(employer => {
-        const employerDiv = document.createElement('div');
-        employerDiv.classList.add('work-item');
-        employerDiv.innerHTML = `
-                            <table>
-                                <tr>
-                                    <td class="col-img"><a href="http://${employer.url}"><img src="images/${employer.image}" alt="${employer.company}"></a></td>
-                                    <td class="col-text">
-                                        <h3>${employer.company}</h3>
-                                        <p>Role: <b>${employer.role}</b></p>
-                                        <p>Duration: ${employer.duration}</p>
-                                        <p>Location: ${employer.location}</p>
-                                        <p>Description: <ul class="details"><li> ${employer.description.replace(/\n/g, "<br><br><li>")} </ul></p>
-                                        
-                                    </td>
-                                </tr>
-                            </table>
-                        `;
-        employersList.appendChild(employerDiv);
-
-    });
-
-*/
+    /*    data.forEach(employer => {
+            const employerDiv = document.createElement('div');
+            employerDiv.classList.add('work-item');
+            employerDiv.innerHTML = `
+                                <table>
+                                    <tr>
+                                        <td class="col-img"><a href="http://${employer.url}"><img src="images/${employer.image}" alt="${employer.company}"></a></td>
+                                        <td class="col-text">
+                                            <h3>${employer.company}</h3>
+                                            <p>Role: <b>${employer.role}</b></p>
+                                            <p>Duration: ${employer.duration}</p>
+                                            <p>Location: ${employer.location}</p>
+                                            <p>Description: <ul class="details"><li> ${employer.description.replace(/\n/g, "<br><br><li>")} </ul></p>
+                                            
+                                        </td>
+                                    </tr>
+                                </table>
+                            `;
+            employersList.appendChild(employerDiv);
+    
+        });
+    
+    */
 }
 
 // Fetch JSON data when the page loads
